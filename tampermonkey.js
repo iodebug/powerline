1a570a5bff3cae7c24f38c680c2c8ffa83df2464
Shaun Mitchell <shaun@shitchell.com>
2018-05-31 19:12:23 -0400

    remove listener after removing original js

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,48 +1,48 @@
 // ==UserScript==
-// @name	powerline.io
-// @namespace	http://tampermonkey.net/
-// @version	0.4
-// @description	Replaces inline JS on powerline.io
-// @author	Shaun Mitchell
-// @match	*://powerline.io/*
-// @grant	unsafeWindow
-// @run-at	document-start
+// @name         powerline.io
+// @namespace    http://tampermonkey.net/
+// @version      0.5
+// @description  Replaces inline JS on powerline.io
+// @author       Shaun Mitchell <guy#4962>
+// @match        *://powerline.io/*
+// @grant        unsafeWindow
+// @run-at       document-start
 // ==/UserScript==
 
 var evilJSRegex = /SpaceWars/;
 var replacementJSUrl = "https://raw.githubusercontent.com/iodebug/powerline/master/powerline.js";
 
 function removeInlineJS(e) {
-	if (evilJSRegex.test(e.target.innerText)) {
-		// Stop the default JS from running
-		e.stopPropagation();
-		e.preventDefault();
+    if (evilJSRegex.test(e.target.innerText)) {
+        // Stop the default JS from running
+        e.stopPropagation();
+        e.preventDefault();
 
-		// Create a new script tag for our custom JS
-		var awesomeJS = document.createElement("script");
-		awesomeJS.type = "text/javascript";
-		awesomeJS.src = replacementJSUrl;
+        // Create a new script tag for our custom JS
+        var awesomeJS = document.createElement("script");
+        awesomeJS.type = "text/javascript";
+        awesomeJS.src = replacementJSUrl;
 
-		// Replace the old JS with our custom shiznit
-		var parentNode = e.target.parentNode;
-		parentNode.removeChild(e.target);
-		parentNode.appendChild(awesomeJS);
+        // Replace the old JS with our custom shiznit
+        var parentNode = e.target.parentNode;
+        parentNode.removeChild(e.target);
+        parentNode.appendChild(awesomeJS);
 
-		// Remove the event listener
-		unsafeWindow.addEventListener("beforescriptexecute", removeInlineJS, true);
-	}
+        // Remove the event listener
+        unsafeWindow.removeEventListener("beforescriptexecute", removeInlineJS, true);
+    }
 }
 
 (function() {
-	'use strict';
-	Object.defineProperty(unsafeWindow, "debug", {
-		get: function() {
-			return true;
-		},
-		set: function(x) {
-			console.log("Cannot change value of debug variable.");
-		}
-	});
+    'use strict';
+    Object.defineProperty(unsafeWindow, "debug", {
+        get: function() {
+            return true;
+        },
+        set: function(x) {
+            console.log("Cannot change value of debug variable.");
+        }
+    });
 
-	unsafeWindow.addEventListener("beforescriptexecute", removeInlineJS, true);
+    unsafeWindow.addEventListener("beforescriptexecute", removeInlineJS, true);
 })();
